2eb2538978a690f01fc08b296b4337f6499cc2a0
Andrey Mozgunov <am0z@ya.ru>
2019-02-14 15:17:26 +0300

    Add list and detail view

--- main.py
+++ main.py
@@ -1,28 +1,63 @@
 #!/usr/bin/env python
 
+import shlex
+
 from sanic import Sanic
 from sanic.response import json
 
-from imbox import Imbox
+from imbox import Imbox, parser
+
+# TODO: flask?
+# TODO: multiuser
 
 app = Sanic()
 client = None
 
-@app.route("/")
-async def root(request):
+@app.route("/<tag>/")
+async def get_list(request, tag):
     global client
     if not client:
         return json({"error": "Please, login"})
     else:
-        # TODO: return only few header fields
-        return json(list(client.messages()))
+        messages = []
+        if client.selected is not tag:
+            for item in client.connection.list()[1]:
+                real_tag = shlex.split(item.decode())[-1]
+                if real_tag.lower().count(tag):
+                    break
+            client.connection.select(real_tag)
+        client.selected = tag
+        uids = client.connection.uid('search', None, '(ALL)')[1][0].split()
+        response = client.connection.uid("fetch", b",".join(uids), "(BODY.PEEK[HEADER.FIELDS (FROM TO DATE SUBJECT)])")
+        for labels, message in response[1][::2]:
+            message = parser.parse_email(message)
+            message.uid = labels.split()[2]
+            messages.append(message)
+        return json(messages)
+
+@app.route("/<tag>/<uid>/")
+async def get_message(request, tag, uid):
+    if client.selected is not tag:
+        for item in client.connection.list()[1]:
+            real_tag = shlex.split(item.decode())[-1]
+            if real_tag.lower().count(tag):
+                break
+        client.connection.select(real_tag)
+    client.selected = tag
+
+    response = client.connection.uid("fetch", uid, "(BODY.PEEK[])")
+    labels, message = response[1][0]
+    message = parser.parse_email(message)
+    del message.raw_email
+    message.uid = labels.split()[2]
+    return json(message)
 
 @app.post('/login')
 async def post_handler(request):
     global client
-    username = request.json["username"]
     password = request.json["password"]
-    client = Imbox("imap.gmail.com", username=username, password=password)
+    client = Imbox("imap.gmail.com", username="andrey@gethappie.me", password=password)
+    client.selected = None
     return json({"success": "logged in"})
 
 if __name__ == "__main__":
