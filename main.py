791dc578ada4ab122521be74818a4d2e4b033459
Andrey Mozgunov <am0z@ya.ru>
2019-03-14 14:54:09 +0300

    Add bunch of improvements
    * Blur iframe
    * Network error indication
    * Login with tag refresh
    * Scroll on iframe load
    * Inline script
    * Fix links to have target
    * Lose hour old clients
    * Fix html decoding
    * Serve text if no html

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ zorro-inbox backend app.
 An Inbox by Google clone
 """
 
+import chardet
 import imaplib
 import shlex
+import time
 
 from sanic import Sanic
 from sanic.response import json, file, text, html
@@ -16,6 +18,8 @@ from imbox import Imbox, parser
 # TODO: flask?
 
 
+iframe_script = """<script src="/inline.js"></script>"""
+
 FLAGS = {
     "seen": "\\Seen",
     "delete": "\\Deleted",
@@ -23,8 +27,25 @@ FLAGS = {
 
 app = Sanic()
 clients = {}
-text_cache = {}
+text_cache = {}  # TODO: cache the whole message maybe
 html_cache = {}
+
+
+def decode_bytes(b: bytes):
+    """
+    Decodes bytes to a string using chardet.  If a string comes in...just returns it.
+    Args:
+        b (bytes): bytes or string
+    """
+    if b is None:
+        return None
+    if isinstance(b, str):
+        return b
+    r = chardet.detect(b)
+    encoding = r.get("encoding") if r else "utf-8"
+    if not encoding:
+        encoding = "utf-8"
+    return b.decode(encoding, errors="ignore")
 
 
 def quote_folder_name(folder_name: str):
@@ -35,7 +56,11 @@ def quote_folder_name(folder_name: str):
 
 @app.route("/api/<username>/<tag>/")
 async def get_list(request, username, tag):
-    client = clients.get(username)
+    global clients
+    client, timestamp = clients.get(username, [None, None])
+    if client and time.time() - timestamp > 60 * 60:  # over an hour
+        del clients[username]
+        client = None
     if not client:
         return json(None, status=401)
     messages = []
@@ -67,12 +92,12 @@ async def get_message_text(request, username, tag, uid):
     global text_cache
     response = text_cache.get(uid)
     if not response:
-        client = clients.get(username)
+        client, _ = clients.get(username)
         response = client.connection.uid("fetch", uid, "(BODY.PEEK[])")
         labels, message = response[1][0]
         message = parser.parse_email(message)
         response = message.body["plain"][0] if message.body["plain"] else "No text"
-        text_cache[uid] = response
+        text_cache[uid] = decode_bytes(response)
     return text(response)
 
 
@@ -81,17 +106,19 @@ async def get_message_html(request, username, tag, uid):
     global html_cache
     response = html_cache.get(uid)
     if not response:
-        client = clients.get(username)
+        client, _ = clients.get(username)
         response = client.connection.uid("fetch", uid, "(BODY.PEEK[])")
         labels, message = response[1][0]
         message = parser.parse_email(message)
-        response = message.body["html"][0]
+        if not message.body["html"] and message.body["plain"]:
+            message.body["html"] = message.body["plain"]
+        response = iframe_script + decode_bytes(message.body["html"][0]) if message.body["html"] else "No html"
     return html(response)
 
 
 @app.delete("/api/<username>/<tag>/<uid>/flags/<flag>/")
 async def delete_flag(request, username, tag, uid, flag):
-    client = clients.get(username)
+    client, _ = clients.get(username)
     flag = FLAGS[flag]
     flags = client.connection.uid("store", uid, "-FLAGS", flag)[1][0]
     if flags is None:
@@ -102,7 +129,7 @@ async def delete_flag(request, username, tag, uid, flag):
 
 @app.post("/api/<username>/<tag>/<uid>/flags/<flag>/")
 async def add_flag(request, username, tag, uid, flag):
-    client = clients.get(username)
+    client, _ = clients.get(username)
     flag = FLAGS[flag]
     flags = client.connection.uid("store", uid, "+FLAGS", flag)[1][0]
     if flags is None:
@@ -117,7 +144,7 @@ async def post_handler(request, username):
     password = request.json["password"]
     client = Imbox("imap.gmail.com", username=username, password=password)
     client.selected = None
-    clients[username] = client
+    clients[username] = client, time.time()
     return json(None, status=200)
 
 
@@ -125,8 +152,12 @@ async def post_handler(request, username):
 @app.route("/<username>")
 @app.route("/<username>/<tag>")
 async def handle_request(request, username=None, tag=None):
-    return await file('index.html')
+    return await file("index.html")
 
+
+@app.route("/inline.js")
+async def handle_request(request, username=None, tag=None):
+    return await file("inline.js")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
